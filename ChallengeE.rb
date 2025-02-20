4d8c8d0a01f19dd8f172bb03250cd5bbb4054af8
mekhiWashington <mekhi_washington@student.davincischools.org>
2025-02-20 13:19:44 -0800

    Create ChallengeE.rb

---
+++ ChallengeE.rb
@@ -0,0 +1,43 @@
+# Challenge E
+use_bpm 180
+use_synth :chipbass
+
+# VARIABLES TO STORE SAMPLES
+intro = "C:/Users/mekhi_washington/Downloads/mario_intro.wav"
+jump = "C:/Users/mekhi_washington/Downloads/jump.wav"
+coin = "C:/Users/mekhi_washington/Downloads/mario_sounds/mario_sounds/mario_sounds/coin.wav"
+oneup = "C:/Users/mekhi_washington/Downloads/mario_sounds/mario_sounds/mario_sounds/oneup.wav"
+
+# INTRO
+play :e4
+sleep 0.5
+play :e4
+sleep 1
+play :e4
+sleep 1
+play :c4
+sleep 0.5
+play :e4
+sleep 1
+play :g4
+sleep 2
+play :g3
+sleep 2
+
+
+# CUSTOM MARIO SAMPLES HERE
+sample intro
+sleep 2
+
+sample jump
+sleep 2
+
+sample coin
+sleep 2
+
+sample oneup
+sleep 2
+
+
+# MUSHROOM SAMPLE HERE
+# https://www.youtube.com/watch?v=6G-k4zxou7Y
