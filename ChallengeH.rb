ea7b60fa5bf4b64eeea339309e186b39719af926
mekhiWashington <mekhi_washington@student.davincischools.org>
2025-03-12 10:50:58 -0700

    Update ChallengeH.rb

--- ChallengeH.rb
+++ ChallengeH.rb
@@ -1,28 +1,56 @@
-use_bpm 120
-use_synth :fm
-use_synth_defaults sustain: 0.25
+# Challenge C
+use_bpm 160
 
-notes = [:e3, :e3, :g3, :e3, :r, :d3, :c3, :b2]
-sleeps = [1.5, 0.5, 0.75, 0.25, 0.5, 0.5, 2, 2]
-
-a = 0
-b = 0
-
-live_loop :white_stripes do
-  8.times do
-    play notes[a] unless a == :r
-    sleep sleeps[b]
-    a += 1
-    b += 1
+define :notesANDfx do |x|
+  with_fx x do
+    play :c2
+    sleep 0.5
+    play :e2
+    sleep 0.5
+    play :g2
+    sleep 0.5
+    play :b2
+    sleep 0.5
+    
+    play :c3
+    sleep 0.5
+    play :b2
+    sleep 0.5
+    play :g2
+    sleep 0.5
+    play :e2
+    sleep 0.5
   end
-  a = 0
-  b = 0
 end
+#change the sound of your notes by using a synthesizer
+use_synth :saw
 
-sleep 8
+play :c2
+sleep 0.5
+play :e2
+sleep 0.5
+play :g2
+sleep 0.5
+play :b2
+sleep 0.5
 
-live_loop :drum do
-  sample :drum_heavy_kick
-  sleep 1
-end
+play :c3
+sleep 0.5
+play :b2
+sleep 0.5
+play :g2
+sleep 0.5
+play :e2
+sleep 0.5
 
+notesANDfx :echo
+notesANDfx :bitcrusher
+notesANDfx :slicer
+notesANDfx :whammy
+# use fx to modify the sound produced by your synth
+
+# sustain just means hold the note longer!
+play :c2, sustain: 3
+play :e2, sustain: 4
+play :g2, sustain: 2
+play :b2, sustain: 2
