3f8892750367fa0dde08a1e34c7646f7ef744dd6
mekhiWashington <mekhi_washington@student.davincischools.org>
2025-04-22 06:53:46 -0700

    Update Runaway.rb

--- Runaway.rb
+++ Runaway.rb
@@ -1,27 +1,28 @@
 use_synth :piano
 use_bpm 80
-#=begin
 
+# Set initial values
+set :playing, true
 amp = 1
-playing = true
 
 with_fx :reverb do
   with_fx :level, amp: amp do |lvl|
+    
+    # Riffs
     riff1 = [:ab3, :e3, :ab3, :b3]
     riff2 = [:ab3, :eb3, :ab3, :b3]
     riff3 = [:e3, :db3, :e3, :ab3]
     riff4 = [:a3, :e3, :db3, :e3]
     
-    
-    
-    AdLib = "C:/Users/mekhi_washington/Downloads/LOOKATYA.wav"
+    # Sample (optional)
+    #AdLib = "C:/Users/mekhi_washington/Downloads/LOOKATYA.wav"
     
     define :intro do |note|
       3.times do
         play note, sustain: 2
         sleep 2
       end
-      play note - 12, sustain: 2  # Play the note an octave lower
+      play note - 12, sustain: 2
       sleep 2
     end
     
@@ -32,10 +33,16 @@ with_fx :reverb do
         play note - 24, sustain: 2
         sleep 1
       end
-      play note - 12, sustain: 2  # Play the note an octave lower
+      play note - 12, sustain: 2
     end
     
-    # Example usage:
+    define :left do |note|
+      play note, sustain: 1
+      sleep 1
+      play note - 24, sustain: 1
+    end
+    
+    # --- Intro ---
     intro :e6
     intro :eb6
     intro :db6
@@ -44,27 +51,13 @@ with_fx :reverb do
       sleep 2
     end
     play :ab5, sustain: 2
-    
-    sample AdLib, amp: 0.75
-    
     sleep 0.5
     play :e6
-    #=end
-    
-    
-    define :left do |note|
-      play note, sustain: 1
-      sleep 1
-      play note - 24, sustain: 1
-    end
-    
-    
     sleep 0.5
     
-    i = 0
-    
+    # --- Bass Loop ---
     live_loop :bass do
-      if playing
+      if get(:playing)
         octaveNotes :e4
         sleep 1
         octaveNotes :e4
@@ -82,57 +75,56 @@ with_fx :reverb do
       else
         stop
       end
-      
     end
     
-    sleep 0.5
-    
+    # --- Riff 1 ---
+    i = 0
     16.times do
       play riff1[i], sustain: 1
-      i += 1
+      i = (i + 1) % 4
       sleep 0.25
-      if i == 4
-        i = 0
-      end
     end
     
+    # --- Riff 2 ---
     16.times do
       play riff2[i], sustain: 1
-      i += 1
+      i = (i + 1) % 4
       sleep 0.25
-      if i == 4
-        i = 0
-      end
     end
     
+    # --- Riff 3 ---
     16.times do
       play riff3[i], sustain: 1
-      i += 1
+      i = (i + 1) % 4
       sleep 0.25
-      if i == 4
-        i = 0
-      end
     end
     
-    
+    # --- Volume Fade & Bass Stop ---
     in_thread do
-      5.times do |i|
-        control lvl, amp: 1 - (i * 0.2)  # gradually reduce volume
-        sleep 1  # time between volume steps
+      5.times do |n|
+        control lvl, amp: 1 - (n * 0.2)
+        sleep 1
       end
-      stop  # stop the fade loop once done
+      set :playing, false
     end
     
-     16.times do
+    # --- Riff 4 ---
+    16.times do
       play riff4[i], sustain: 1
-      i += 1
+      i = (i + 1) % 4
       sleep 0.25
-      if i == 4
-        i = 0
-      end
     end
-    stop
+    
     control lvl, amp: 0
-    print(lvl)
+    
+    sleep 0.1
+    
+    control lvl, amp: 1
+    
+    3.times do
+      play :e3
+      sleep 1
+    end
+    
   end
 end
